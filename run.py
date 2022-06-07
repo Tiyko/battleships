4b69066a5e6031f7ff9cf1f43935ff34a9ef0911
Tiyko <johnny86shepherd@gmail.com>
2022-06-07 19:30:28 +0000

    Add functions to be used inside the classes.

--- run.py
+++ run.py
@@ -4,9 +4,24 @@
 class GameBoard:
     """Gameboard class, builds an 8 by 8 board"""
 
+    def get_letters_to_numbers(self):
+        """Sets the X and Y axis"""
+
+    def print_board(self):
+        """Prints the board"""
+
 
 class Battleship:
     """Class that builds and randomizes the battleships on the board"""
+
+    def create_ships(self):
+        """Ramdomizes the ships on the board"""
+
+    def get_user_input(self):
+        """Gets the user input and prints the result"""
+
+    def count_hit_ships(self):
+        """Counts the number of ships and marks it on the board"""
 
 
 def run_game():
