97796d33a365b02b9c44d980009cd4449beffef1
Tiyko <johnny86shepherd@gmail.com>
2022-06-07 19:49:32 +0000

    Created the game board.

--- run.py
+++ run.py
@@ -4,11 +4,24 @@
 class GameBoard:
     """Gameboard class, builds an 8 by 8 board"""
 
+    def __init__(self, board):
+        self.board = board
+
     def get_letters_to_numbers(self):
         """Sets the X and Y axis"""
+        letters_to_numbers = {"A": 0, "B": 1, "C": 2,
+                              "D": 3, "E": 4, "F": 5, "G": 6, "H": 7}
+        return letters_to_numbers
 
     def print_board(self):
         """Prints the board"""
+        print("  ")
+        print("  A B C D E F G H")
+
+        row_number = 1
+        for row in self.board:
+            print("%d|%s|" % (row_number, "|".join(row)))
+            row_number += 1
 
 
 class Battleship:
@@ -31,6 +44,15 @@ def run_game():
     print("There are 20 ships hidden on a board of 64 slots.")
     print("You have 20 bullets. How many ships can you bring down?")
     print(" ")
+    computer_board = GameBoard([[" "] * 8 for i in range(8)])
+    user_guess_board = GameBoard([[" "] * 8 for i in range(8)])
+    Battleship.create_ships(computer_board)
+
+    # start 20 turns
+    bullets = 20
+    print(bullets, "bullets.")
+    while bullets > 0:
+        GameBoard.print_board(user_guess_board)
 
 
 run_game()
