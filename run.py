47ea82400888827f6791474e8875f291697dd235
Tiyko <johnny86shepherd@gmail.com>
2022-06-07 19:27:29 +0000

    Add initial classes and functions

--- run.py
+++ run.py
@@ -1,3 +1,21 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""Import modules"""
+
+
+class GameBoard:
+    """Gameboard class, builds an 8 by 8 board"""
+
+
+class Battleship:
+    """Class that builds and randomizes the battleships on the board"""
+
+
+def run_game():
+    """Function that runs the game"""
+    print(" ")
+    print("This is a battleship game!")
+    print("There are 20 ships hidden on a board of 64 slots.")
+    print("You have 20 bullets. How many ships can you bring down?")
+    print(" ")
+
+
+run_game()
